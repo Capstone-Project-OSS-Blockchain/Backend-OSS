123609d468abb1bc4e8e413ecf7a8b910887561e
Rhizky Zain Nabil Subiyantara <rhizky.subiyantara@gmail.com>
2023-08-22 20:03:48 +0700

    add generate PDF on upload

--- handlers/upload/upload.go
+++ handlers/upload/upload.go
@@ -1,53 +1,49 @@
 package upload
 
 import (
-	"fmt"
-	"net/http"
+    "bytes"
+    "fmt"
+    "net/http"
+    _ "os"
 
-	"github.com/minio/minio-go/v7"
-	_ "github.com/minio/minio-go/v7/pkg/credentials"
+    "github.com/google/uuid"
+    "github.com/minio/minio-go/v7"
+    _ "github.com/minio/minio-go/v7/pkg/credentials"
 
-	minioconnections "github.com/Capstone-Project-OSS-Blockchain/Backend-OSS/connections"
+    minioconnections "github.com/Capstone-Project-OSS-Blockchain/Backend-OSS/connections"
 )
 
 func UploadHandler(w http.ResponseWriter, r *http.Request) {
-	// Parse the request
-	err := r.ParseMultipartForm(1024)
-	if err != nil {
-		http.Error(w, "Unable to parse form", http.StatusBadRequest)
-		return
-	}
+    minioClient, err := minioconnections.InitMinIOClient()
+    if err != nil {
+        http.Error(w, "Failed to initialize MinIO client", http.StatusInternalServerError)
+        return
+    }
 
-	minioClient, err := minioconnections.InitMinIOClient()
-	if err != nil {
-		http.Error(w, "Failed to initialize MinIO client", http.StatusInternalServerError)
-		return
-	}
+    // Generate a UUID for the filename
+    filename := uuid.New().String() + ".pdf"
 
-	// Loop through uploaded files
-	for _, fileHeaders := range r.MultipartForm.File {
-		for _, fileHeader := range fileHeaders {
-			file, err := fileHeader.Open()
-			if err != nil {
-				http.Error(w, "Failed to open uploaded file", http.StatusInternalServerError)
-				return
-			}
-			defer file.Close()
+    // Create a buffer to store PDF content
+    pdfBuffer := new(bytes.Buffer)
 
-			// Upload the file to MinIO
-			bucketName := "dzaky-upload"
-			//bucket name diubah menggunakan bucket local, tapi tetap masih fail
-			objectName := fileHeader.Filename
+    // Here you would add code to generate the PDF content using a library like gopdf or gofpdf.
+    // For demonstration purposes, let's assume we are writing some basic content to the PDF.
+    pdfBuffer.WriteString("This is a generated PDF content.")
 
-			_, err = minioClient.PutObject(r.Context(), bucketName, objectName, file, fileHeader.Size, minio.PutObjectOptions{
-				ContentType: fileHeader.Header.Get("Content-Type"),
-			})
-			if err != nil {
-				http.Error(w, "Error uploading file to MinIO", http.StatusInternalServerError)
-				return
-			}
-		}
-	}
+    // Upload the generated PDF buffer to MinIO
+    bucketName := "wasabi-bucket"
+    objectName := filename
 
-	fmt.Fprintln(w, "Files uploaded successfully!")
+    _, err = minioClient.PutObject(r.Context(), bucketName, objectName, pdfBuffer, int64(pdfBuffer.Len()), minio.PutObjectOptions{
+        ContentType: "application/pdf",
+    })
+    if err != nil {
+        http.Error(w, "Error uploading PDF file to MinIO", http.StatusInternalServerError)
+        return
+    }
+
+    fmt.Fprintln(w, "PDF file generated and uploaded successfully!")
+
+    // Close the buffer
+    defer pdfBuffer.Reset()
 }
