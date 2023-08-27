26b9828bd59404c141aedf72e70184c0cf6dd7c8
Fairuzj <fairuzjmf@gmail.com>
2023-08-27 16:01:00 +0700

    cek

--- main.go
+++ main.go
@@ -27,7 +27,7 @@ func main() {
 	connections.Connect()
 
 	r := mux.NewRouter()
-	//ini bini
+	//ini
 	corsHandler := cors.New(cors.Options{
 		AllowedHeaders: []string{"*"},
 		AllowedOrigins: []string{"*"},                                     // You can specify specific origins here or use "*" for all origins
@@ -41,6 +41,7 @@ func main() {
 	ownership.InitDB(connections.DB)
 	r.HandleFunc("/register", auth.RegisterUser).Methods("POST")
 	r.HandleFunc("/login", auth.Login).Methods("POST")
+
 	// r.HandleFunc("/upload", upload.UploadHandler).Methods("POST")
 	//r.HandleFunc("/download/{filename}", download.DownloadHandler).Methods("GET")
 	//r.HandleFunc("/protected", auth.ProtectedRoute).Methods("GET").Handler(auth.AuthMiddleware(http.HandlerFunc(auth.ProtectedRoute)))
